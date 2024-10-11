6c4527bb8fb9314a2ae715bdec23d2945ce2b7a6
ican0422 <ican0422@gmail.com>
2024-10-11 10:41:29 +0900

    Level 3-3 : 헬스체크

---
+++ src/main/java/org/example/expert/domain/common/controller/HealthCheckController.java
@@ -0,0 +1,15 @@
+package org.example.expert.domain.common.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class HealthCheckController {
+
+    @GetMapping("check")
+    public String getCheck() {
+        return "Server is Up";
+    }
+}
